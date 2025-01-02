ad01d6d4feb82cf0c1bf940232aeecc71421e3e9
Hailemeskel Getaneh <hailegetaneh1221@gmail.com>
2025-01-02 23:15:02 +0300

    backend setupe for connecting it with frontendd

---
+++ week4/4backendFrontendIntergration/backend/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose  from 'mongoose';
+import dotenv from 'dotenv'
+
+const app = express();
+
+app.use(cors());
+app.use(express.json())
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+const MONGOURL= process.env.MONGOURL;
+
+mongoose.connect(MONGOURL)
+.then(() => {
+    console.log("Database Connected Successfully")
+})
+
+.catch(err => console.log("Database connection failed"))
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
