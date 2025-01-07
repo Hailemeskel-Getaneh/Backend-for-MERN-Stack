0963b0b75304b91927f73372a0f13a451a19e916
Hailemeskel Getaneh <hailegetaneh1221@gmail.com>
2025-01-07 19:57:45 +0300

    setuping nodemnailer for email transporting

--- week4/3SendNotifatinViaEmail/Backend/index.js
+++ week4/3SendNotifatinViaEmail/Backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import nodemailer from 'nodemailer'
 
 
 const app = express()
@@ -9,7 +10,17 @@ app.use(cors());
 
 
 const PORT = 4000;
+app.use(bodyParser.json());
+app.use(express.static('public')); // Serve static files (like your React build)
 
+// Nodemailer transporter setup
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`)
